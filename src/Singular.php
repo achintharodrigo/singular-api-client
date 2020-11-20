e897fd67f14ae22a097ec291d3df3ed536b8b674
Achintha Rodrigo <achintha@gmail.com>
2020-11-20 13:41:01 +0530

    :art: refactor code

--- src/Singular.php
+++ src/Singular.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Http;
 
 class Singular
 {
-    private $baseUrl = '';
+    private $baseUri = '';
     private $apiKey = '';
     private $request;
 
@@ -28,7 +28,7 @@ class Singular
             'format' => $format,
             'display_non_active_sources' => $showNonActive ? 'true' : 'false'
         ]);
-        return $this->format_response($response);
+        return $this->formatResponse($response);
     }
 
     public function getReportStatus($id)
@@ -36,35 +36,42 @@ class Singular
         $response = $this->request->get($this->baseUri . 'v2.0/get_report_status', [
             'report_id' => $id,
         ]);
-        return $this->format_response($response);
+        return $this->formatResponse($response);
     }
 
     public function getFilters()
     {
         $response = $this->request->get($this->baseUri . 'v2.0/reporting/filters');
-        return $this->format_response($response);
+        return $this->formatResponse($response);
     }
 
     public function getCustomDimensions()
     {
         $response = $this->request->get($this->baseUri . 'custom_dimensions');
-        return $this->format_response($response);
+        return $this->formatResponse($response);
     }
 
     public function getCohortMetrics()
     {
         $response = $this->request->get($this->baseUri . 'cohort_metrics');
-        return $this->format_response($response);
+        return $this->formatResponse($response);
     }
 
     public function getConversionMetrics()
     {
         $response = $this->request->get($this->baseUri . 'conversion_metrics');
-        return $this->format_response($response);
+        return $this->formatResponse($response);
     }
 
-    public function getAdMonatizationReport($start, $end, $dimensions = [], $metrics = [], $format = 'json', $timeBreakDown = 'all', $countryCodeFormat = 'iso3')
-    {
+    public function getAdMonetizationReport(
+        $start,
+        $end,
+        $dimensions = [],
+        $metrics = [],
+        $format = 'json',
+        $timeBreakDown = 'all',
+        $countryCodeFormat = 'iso3'
+    ) {
         $start = Carbon::parse($start)->format('Y-m-d');
         $end = Carbon::parse($end)->format('Y-m-d');
         $response = $this->request->get($this->baseUri . 'v2.0/admonetization/reporting', [
@@ -76,13 +83,13 @@ class Singular
             'time_breakdown' => $timeBreakDown,
             'country_code_format' => $countryCodeFormat
         ]);
-        return $this->format_response($response);
+        return $this->formatResponse($response);
     }
 
     public function getApps()
     {
         $response = $this->request->get($this->baseUri . 'v1/links/discover_apps');
-        return $this->format_response($response);
+        return $this->formatResponse($response);
     }
 
     public function getAvailablePartners($appId)
@@ -90,7 +97,7 @@ class Singular
         $response = $this->request->get($this->baseUri . 'v1/links/discover_available_partners', [
             'singular_app_id' => $appId,
         ]);
-        return $this->format_response($response);
+        return $this->formatResponse($response);
     }
 
     public function getLinks($appId = '', $partnerId = '', $linkId = '', $includeArchived = true)
@@ -101,7 +108,7 @@ class Singular
             'tracking_link_ids' => $linkId,
             'include_archived_links' => $includeArchived ? 'true' : 'false'
         ]);
-        return $this->format_response($response);
+        return $this->formatResponse($response);
     }
 
     public function getCustomLinks($appId = '', $customSource = '', $linkId = '', $includeArchived = true)
@@ -112,14 +119,21 @@ class Singular
             'tracking_link_ids' => $linkId,
             'include_archived_links' => $includeArchived ? 'true' : 'false'
         ]);
-        return $this->format_response($response);
+        return $this->formatResponse($response);
     }
 
-    public function createReport($start, $end, $dimensions = [], $metrics = [], $format = 'json', $timeBreakDown = 'all', $countryCodeFormat = 'iso3')
-    {
+    public function createReport(
+        $start,
+        $end,
+        $dimensions = [],
+        $metrics = [],
+        $format = 'json',
+        $timeBreakDown = 'all',
+        $countryCodeFormat = 'iso3'
+    ) {
         $start = Carbon::parse($start)->format('Y-m-d');
         $end = Carbon::parse($end)->format('Y-m-d');
-        $response = $this->request->post($this->baseUri . 'v2.0/create_async_report')
+        return $this->request->post($this->baseUri . 'v2.0/create_async_report')
             ->body([
                 'start_date' => $start,
                 'end_date' => $end,
@@ -129,7 +143,6 @@ class Singular
                 'time_breakdown' => $timeBreakDown,
                 'country_code_format' => $countryCodeFormat
             ]);
-        return $response;
     }
 
     public function getReportData($id)
@@ -149,7 +162,7 @@ class Singular
         return $jsonData;
     }
 
-    private function format_response($response)
+    private function formatResponse($response)
     {
         return $response->json();
     }
